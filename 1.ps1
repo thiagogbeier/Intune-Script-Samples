8b8eefd55036131a1cade3710868476c05548f23
Thiago Beier <146458544+thiagogbeier@users.noreply.github.com>
2025-05-16 11:40:55 -0400

    Create 1.ps1

---
+++ 1.ps1
@@ -0,0 +1,15 @@
+# Define the path to your script
+$scriptPath = ".\Test-IntuneConnectivity.ps1"
+
+# Start the timer
+$startTime = Get-Date
+
+# Run the script
+& $scriptPath
+
+# Stop the timer
+$endTime = Get-Date
+
+# Calculate and display the elapsed time
+$elapsedTime = $endTime - $startTime
+Write-Host "Script execution time: $($elapsedTime.TotalSeconds) seconds"
